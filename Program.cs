a4766c93097fc887faa2059fae3d33c290445510
Ivan Novak <77879145+ivan-novak@users.noreply.github.com>
2021-03-10 19:57:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            while (true)
+            {
+                string s = Console.ReadLine();
+                if (s == "") break;
+                if(s.GroupBy(c => c).Where(c => c.Count() == 2).Count() > 0) Console.WriteLine(s);
+            }             
+        }
+    }
+}
